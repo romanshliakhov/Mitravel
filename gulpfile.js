509f38045c2e80025671e1a68bff3ad5ee0345ed
romanshliakhov <romanshliakhov@gmail.com>
2021-10-23 19:29:06 +0300

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,13 @@ const webp = require("gulp-webp");
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
 const svgstore = require("gulp-svgstore");
+const fileinclude = require("gulp-file-include");
 const posthtml = require("gulp-posthtml");
 const include = require("posthtml-include");
 const del = require("del");
+const uglify = require('gulp-uglify-es').default;
 const htmlmin = require("gulp-htmlmin");
+// const purgecss = require('gulp-purgecss')
 
 gulp.task("css", () => {
   return gulp.src("source/sass/style.scss")
@@ -29,12 +32,15 @@ gulp.task("css", () => {
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
+
 });
+
 
 gulp.task("js", () => {
   return gulp.src("source/js/**/*.js")
     .pipe(plumber())
     .pipe(sourcemap.init())
+    .pipe(uglify())
     .pipe(gulp.dest("build/js"))
     .pipe(rename(function (path) {
       path.extname = ".min.js";
@@ -96,12 +102,24 @@ gulp.task("sprite", () => {
     .pipe(gulp.dest("build/img/sprite"));
 });
 
+// gulp.task("html", () => {
+//   return gulp.src("source/*.html")
+//     .pipe(posthtml([
+//       include()
+//     ]))
+//     // .pipe(htmlmin({ collapseWhitespace: true }))
+//     .pipe(gulp.dest("build"));
+// });
+
 gulp.task("html", () => {
   return gulp.src("source/*.html")
+    .pipe(fileinclude())
     .pipe(posthtml([
       include()
     ]))
-    .pipe(htmlmin({ collapseWhitespace: true }))
+    // .pipe(htmlmin({
+    //   collapseWhitespace: false
+    // }))
     .pipe(gulp.dest("build"));
 });
 
